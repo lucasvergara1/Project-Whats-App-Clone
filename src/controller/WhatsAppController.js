3300d5d8eda455ee25937f3a0fb2d250ec85ce0a
Lucas <lucas.vergara@live.com>
2024-02-06 23:24:05 -0300

    :sparkles: Create events for open/close profile edit and add contact

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -75,12 +75,23 @@ class WhatsAppController {
 
         this.el.myPhoto.on('click', e => {
 
-            this.el.panelEditProfile.addClass('open');
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open')
+            }, 300);
+
 
 
         });
 
         this.el.btnNewContact.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open')
+            }, 300);
 
             this.el.panelAddContact.addClass('open');
 
@@ -93,9 +104,19 @@ class WhatsAppController {
 
         });
 
+        this.el.btnClosePanelAddContact.on('click', e=> {
 
+            this.el.panelAddContact.removeClass('open');
+
+        });
 
     }
 
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
+    }
 
 }
